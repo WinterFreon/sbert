ffe52639180ae629c17c72b5c1f83ea52d2357d3
WinterFreon <153367716+WinterFreon@users.noreply.github.com>
2025-08-19 02:50:59 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+config = {
+    'model_name': 'bert-base-uncased',
+    'embedding_dim': 768,
+    'num_labels': 3,
+    'batch_size': 16,
+    'num_epochs': 1,
+    'lr': 2e-5,
+    'device': 'cuda'  # or 'cpu'
+}
